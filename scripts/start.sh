2c8a969fc6f41f57f86fe52bb761364fd91527b2
leon <leon860516@gmail.com>
2017-01-10 19:20:43 +0800

    fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -106,7 +106,7 @@ if [[ "$RUN_SCRIPTS" == "1" ]] ; then
     # make scripts executable incase they aren't
     chmod -Rf 750 /var/www/html/scripts/*.sh
     # run scripts in number order
-    for i in `ls /var/www/html/scripts/*.sh`; do /var/www/html/scripts/$i ; done
+    for i in `ls /var/www/html/scripts/*.sh`; do $i ; done
   else
     echo "Can't find script directory"
   fi
