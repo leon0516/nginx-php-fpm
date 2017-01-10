2c2c776c78ed78c8a5b466bdd15b0f6313ce84fc
leon <leon860516@gmail.com>
2017-01-10 19:00:08 +0800

    update

--- scripts/start.sh
+++ scripts/start.sh
@@ -104,9 +104,9 @@ chown -Rf nginx.nginx /var/www/html
 if [[ "$RUN_SCRIPTS" == "1" ]] ; then
   if [ -d "/var/www/html/scripts/" ]; then
     # make scripts executable incase they aren't
-    chmod -Rf 750 /var/www/html/scripts/*
+    chmod -Rf 750 /var/www/html/scripts/*.sh
     # run scripts in number order
-    for i in `ls /var/www/html/scripts/`; do /var/www/html/scripts/$i ; done
+    for i in `ls /var/www/html/scripts/*.sh`; do /var/www/html/scripts/$i ; done
   else
     echo "Can't find script directory"
   fi
